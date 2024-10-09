fceca04f0d2e0629b86167554ef5f0740c392ffa
Kesh <59233228+Kezia-Cane@users.noreply.github.com>
2024-10-09 10:14:29 +0800

    added icons in list view

---
+++ pratice_flutter/lib/random_words.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
+
+  Widget _buildList() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, item) {
+          if (item.isOdd) return Divider();
+
+          final index = item ~/ 2;
+          if (index >= _randomWordPairs.length) {
+            _randomWordPairs.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_randomWordPairs[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
+
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 18.0),
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+    );
+  }
+
+  Widget build(buildContext) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          title: Text('Startup Name Generator'),
+        ),
+        body: _buildList());
+  }
+}
